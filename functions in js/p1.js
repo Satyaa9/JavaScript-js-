d68119fdfba2bb4e84f847b11451770902f0ac0f
Satish <dadassatish6@gmail.com>
2025-10-12 07:28:20 +0530

    new

---
+++ functions in js/p1.js
@@ -0,0 +1,11 @@
+function fun(a,b){
+	console.log(a);
+}
+fun(10,20);
+
+/*
+OUTPUT:
+10
+
+Variable argument passing using function.
+*/
---
+++ functions in js/p2.js
@@ -0,0 +1,14 @@
+function fun(a,b){
+	console.log("in fun", a);
+	return 10;
+}
+a = fun(10,20)
+console.log(a);
+
+/*
+OUTPUT:
+in fun 10
+10
+
+Function madhun value return pn karu shakato.
+*/
