2c14df6b419d6652609acd6a73c254a1db2e9783
Satish <dadassatish6@gmail.com>
2025-10-19 20:20:50 +0530

    new

---
+++ Synchronous programming/p1.js
@@ -0,0 +1,15 @@
+console.log("task 1")
+console.log("task 2")
+console.log("task 3")
+
+/*
+OUTPUT:
+task 1
+task 2
+task 3
+
+Synchronous programming mhaje code line by line execute hoto aani to parent 1st task execute hot nahi toprent dusrya task perform hot nahi 
+aani jr task1 la jr jast time lagat asel tr toparent dusre task block hotat.
+
+programming madhe pratyek language by-default synchronous aste.
+*/
---
+++ Synchronous programming/p2.js
@@ -0,0 +1,25 @@
+console.log("task 1")
+
+for(let i=0; i<10; i++){
+	console.log(i)
+}
+
+console.log("task 2")
+
+/*
+OUTPUT:
+task 1
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+task 2
+
+jo parent sequince val execution complete hot nahi to-parent khalchya process block krt.
+*/
