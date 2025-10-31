fa25de572c7e818be0dac75c52f81d085ba95f8b
Satish <dadassatish6@gmail.com>
2025-10-31 23:27:41 +0530

    new

---
+++ Template in js/p1.js
@@ -0,0 +1,10 @@
+let name = "shubham"
+let data = `my name is ${name}`
+console.log(data)
+
+/*
+OUTPUT:
+my name is shubham
+
+` : BACKSTRICK KEY
+*/
---
+++ Template in js/p2.js
@@ -0,0 +1,14 @@
+let name = "satish"
+let data = `my name 
+
+	is ${name}`
+console.log(data)
+
+/*
+OUTPUT:
+my name
+
+        is satish
+
+String multiple lines madhe pn lihu shakato.
+*/
